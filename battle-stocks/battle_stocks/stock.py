09f712ae7f8ea2e0bb51d292ad75af300606b20c
Ian Cargill <idcargill@gmail.com>
2022-02-15 22:15:47 -0800

    WIP: Stock Class

--- battle-stocks/battle_stocks/stock.py
+++ battle-stocks/battle_stocks/stock.py
@@ -5,11 +5,11 @@ def get_current_stock_price(symbol):
     return { 'price' : 100 }
 
 class Stock:
-    def __init__(self, name, symbol, created=None):
+    def __init__(self, name, symbol, created=None, price=None):
         self.name = name
         self.symbol = symbol
-        self.purchased_date =  created or datetime.datetime.now().date()
         self.purchased_price = self.set_initial_purchased_price()
+        self.purchased_date =  created or datetime.datetime.now().date()
         self.current_price = get_current_stock_price(self.symbol)
         
 
@@ -20,7 +20,7 @@ class Stock:
     def get_price_diff(self):
         current = get_current_stock_price(self.symbol)
         current_price = current['price']
-        return self.purchased_price - current_price        
+        return self.purchased_price - current_price       
 
     # def set_date_purchased(self):
     #     if self.initialized == 0:
