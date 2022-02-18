5c4ef05c18c435c8177fcd3c472e3114e9af035d
Fadab2 <ali_berg1@yahoo.com>
2022-02-17 21:48:03 -0800

    add and remove function done

--- battle-stocks/battle_stocks/classes/portfolio.py
+++ battle-stocks/battle_stocks/classes/portfolio.py
@@ -1,17 +1,24 @@
+from battle_stocks.classes.transaction import Transaction
+from battle_stocks.classes.user import User
+
 class Portfolio:
     def __init__(self):
-        stocks = []
-        past_stocks = []
-        self.current_value = current_value
+        self.stocks = []
+        self.past_stocks = []
+        self.portfolio_value = portfolio_value
 
-    def add_stock(self):
+    def add_stock(self, symbol, qty):
+        add_stock = Transaction.buy_stock(symbol, qty)
+        self.stocks.append(add_stock)
+       
+    def remove_stock(self, symbol, qty):
+        remove_stock = Transaction.sell_stock(symbol, qty)
+        if symbol not in self.stocks:
+            print(f'You do not have {symbol} in your portfolio')
+        else:
+            self.stocks.pop(remove_stock)
 
-        pass
-
-    def remove_stock():
-        pass
-
-    def plot_single_stock():
+    def plot_single_stock(self):
         pass
 
     def plot_portfolio():
@@ -21,8 +28,15 @@ class Portfolio:
         pass
 
     def get_portfolio_value(self, user):
-        pass
+        # need to add total values and stocks
+        return self.portfolio_value
 
     def gain_loss(self):
         pass
 
+# if __name__ == "__main__":
+#     portfo = Portfolio()
+#     added = portfo.add_stock('APL')
+#     removed = portfo.remove_stock('APL')
+#     print(added)
+#     print(removed)
