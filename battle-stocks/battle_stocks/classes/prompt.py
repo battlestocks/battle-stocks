3788d5d3cfa6e048fe8ed2acf91d70e345afe48b
brandonmizutani <bran2miz@gmail.com>
2022-02-19 08:52:46 -0800

    added some more prompts and notes

--- battle-stocks/battle_stocks/classes/prompt.py
+++ battle-stocks/battle_stocks/classes/prompt.py
@@ -47,7 +47,8 @@ Too Bad! Lets invest soon!''')
 Now that you are intersted, you can either buy or sell stocks!
 
 To buy stocks please enter: (b)uy
-To sell stocks pleaser enter: (s)ell
+To sell stocks please enter: (s)ell
+To quit please enter: (q)uit
             ''')
         # user_stock_input = input('> ')
         # if user_stock_input == 'b':
@@ -76,26 +77,41 @@ Unfortunately, you currently do not have enough money to buy a stock. Please ent
         print(
             f'''
 Congratulations! You can purchase {symbol} stock. Would you like to purchase it?
-                '''
-        )
+                ''')
     # yes or no
         user_input = input('< ')
         if user_input == 'n':
             print(
                 '''
-Please enter another stock you would like to purchase.
-                '''
-            )
-
-            #
+Please enter another stock you would like to purchase or enter (q)uit.
+                ''')
+        # else the user can take out money from the bank and use it to purchase the stock.
+        # the user then can store stocks and get the info of its name, ticker, price.
+        # or the user can type in quit and it will return to show()
 
 
 # static method for sell stock
 
     @staticmethod
     def sell_stock():
-        pass
-
+        # first print out the tickers of the stocks that they have stored.
+        print(
+            '''
+Which stock would you like to sell?''')
+        user_input = input('> ')
+        if user_input == SYMBOL:
+            # take that stock from the stored stocks and sell it.
+            # add money to the bank
+            print(
+                f'''
+{SYMBOL} has successfully been sold! Would you like to sell another?  
+                '''
+            )
+        continue_input = input('> ')
+        if continue_input == 'n':
+            Prompt.show()
+        elif continue_input == 'y':
+            Prompt.sell_stock()
 
 if __name__ == "__main__":
     Prompt.stock_greetings()
