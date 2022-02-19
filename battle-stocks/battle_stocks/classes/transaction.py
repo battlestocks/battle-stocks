9706f49b8c9239bb7f5397f297b3ee679f4d2245
Ian Cargill <idcargill@gmail.com>
2022-02-18 18:42:53 -0800

    IC-plot: Plotting class with example file.

--- battle-stocks/battle_stocks/classes/transaction.py
+++ battle-stocks/battle_stocks/classes/transaction.py
@@ -2,16 +2,14 @@ import datetime
 from battle_stocks.utils.scraping import get_current_stock_price
 from battle_stocks.utils.constants import SYMBOL
 
-# Place holder Tester
-# def get_current_stock_price(symbol):
-#   return '75.86'
 
 class Transaction:
-  def __init__(self, symbol, qty, price):
+  def __init__(self, symbol, qty):
     self.symbol = symbol
     self.qty = qty
-    self.purchased_price = price
+    self.purchased_price = get_current_stock_price(symbol)
     self.purchased_date = datetime.datetime.now().date()
+    self.price_history = {}
 
   def sell_stock(self, qty):
       current_price = float(get_current_stock_price(self.symbol))
@@ -24,26 +22,33 @@ class Transaction:
     return float(current_price)
 
   def get_current_value(self):
-    return self.qty * self.current_price
+    return self.qty * self.get_current_price()
   
   def current_total_value(self):
     current_price = float(get_current_stock_price(self.symbol))
     return current_price * self.qty
+
+  def get_price_history(self):
+    # Need function to get price history data { date: price }
+    # data = float(get_price_history(self.symbol))
+    # self.price_history = data
+    d1 = datetime.date.fromisoformat('2022-02-13')
+    d2 = datetime.date.fromisoformat('2022-02-14')
+    d3 = datetime.date.fromisoformat('2022-02-15')
+    d4 = datetime.date.fromisoformat('2022-02-16')
+    d5 = datetime.date.fromisoformat('2022-02-17')
+    self.price_history = { d1: 50, d2: 52, d3: 55, d4: 60, d5: 62 }
+
+  def get_price_history2(self):
+    d1 = datetime.date.fromisoformat('2022-02-13')
+    d2 = datetime.date.fromisoformat('2022-02-14')
+    d3 = datetime.date.fromisoformat('2022-02-15')
+    d4 = datetime.date.fromisoformat('2022-02-16')
+    d5 = datetime.date.fromisoformat('2022-02-17')
+    self.price_history = { d1: 77, d2: 88, d3: 80, d4: 76, d5: 79 }
  
   @staticmethod
   def buy_stock(symbol, qty):
     # Returns a Transaction instance
     price = float(get_current_stock_price(symbol))
     return Transaction(symbol, qty, price)
-
-
-
-## Sample Output: 
-# apple_stock = Transaction.buy_stock(SYMBOL['APPLE'], 20)
-# print(apple_stock.current_total_value()) # 3451.0
-# sold_value = apple_stock.sell_stock(10) 
-# print(sold_value) # 1725.5
-# print(apple_stock.qty)  #10
-# print(apple_stock.current_total_value()) #1725.5
-# print(apple_stock.purchased_price) # 172.55
-# print(apple_stock.purchased_date) # 2022-02-16
