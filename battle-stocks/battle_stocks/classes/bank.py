985d812a247ba9c42df42dc6f7b7d34c45952c3b
royce79-creator <connorsboyce07@gmail.com>
2022-02-19 15:46:30 -0800

    Tweaking the Banking class

--- battle-stocks/battle_stocks/classes/bank.py
+++ battle-stocks/battle_stocks/classes/bank.py
@@ -2,15 +2,14 @@ class Bank:
     def __init__(self, balance=10000):
         self.balance = balance 
         
-    def deposit(self, amount: int):
+    def deposit(self, amount):
         self.balance += amount
 
-    def withdraw(self, amount: int):
+    def withdraw(self, amount):
         if self.balance < amount:
             print('Not enough money, try another amount!')
         else:
             self.balance -= amount
 
     def get_balance(self):
-        if self.balance >= 0:
-            return self.balance
+        return round(self.balance, 2)
